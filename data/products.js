674789d41cd9785221a39f0df21e2b4530276e96
QuocThinh0411 <116668292+QuocThinh0411@users.noreply.github.com>
2024-07-30 14:12:50 +0700

    update
    update

---
+++ data/products.js
@@ -0,0 +1,152 @@
+var list_products = [{
+    "name": "PHIM ĐIỆN ẢNH THÁM TỬ LỪNG DANH CONAN: NGÔI SAO 5 CÁNH 1 TRIỆU ĐÔ",
+    "company": "CGV",
+    "img": "img/products/1.jpg",
+    "price": "150.000",
+    "star": 4,
+    "rateCount": 26,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+       
+    },
+    "masp": "conan"
+
+}, {
+    "name": "Dự Án Mật: Thảm Hoạ Trên Cầu",
+    "company": "CGV",
+    "img": "img/products/2.jpg",
+    "price": "200.000",
+    "star": 5,
+    "rateCount": 26,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        
+    },
+    "masp": "duanmat"
+}, {
+    "name": "EXHUMA: QUẬT MỘ TRÙNG MA",
+    "company": "CGV",
+    "img": "img/products/3.jpg",
+    "price": "200.000",
+    "star": 5,
+    "rateCount": 188,
+    "promo": {
+        "name": "giamgia",
+        "value": "10.000"
+    },
+    "detail": {
+        
+    },
+    "masp": "exhuma"
+}, {
+    "name": "PHIM ĐIỆN ẢNH DORAEMON: NOBITA VÀ BẢN GIAO HƯỞNG ĐỊA CẦU",
+    "company": "GalaxyCinema",
+    "img": "img/products/4.jpg",
+    "price": "200.000",
+    "star": 5,
+    "rateCount": 7,
+    "promo": {
+        "name": "giamgia",
+        "value": "10.000"
+    },
+    "detail": {
+        
+    },
+    "masp": "doraemon"
+}, {
+    "name": "KẺ TRỘM MẶT TRĂNG 4",
+    "company": "LotteCinema",
+    "img": "img/products/5.jpg",
+    "price": "150.000",
+    "star": 5,
+    "rateCount": 0,
+    "promo": {
+        "name": "giamgia",
+        "value": "10.000"
+    },
+    "detail": {
+        
+    },
+    "masp": "minion"
+}, {
+    "name": "NHỮNG MẢNH GHÉP CẢM XÚC 2",
+    "company": "LotteCinema",
+    "img": "img/products/6.jpg",
+    "price": "150.000",
+    "star": 4,
+    "rateCount": 10,
+    "promo": {
+        "name": "giareonline",
+        "value": "10.000"
+    },
+    "detail": {
+       
+    },
+    "masp": "insideout"
+}, {
+    "name": "CỬU LONG THÀNH TRẠI: VÂY THÀNH",
+    "company": "CGV",
+    "img": "img/products/7.jpg",
+    "price": "200.000",
+    "star": 4,
+    "rateCount": 10,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+       
+    },
+    "masp": "cuulong"
+}, {
+    "name": "Nhà bà Nữ",
+    "company": "GalaxyCinema",
+    "img": "img/products/8.jpg",
+    "price": "200.000",
+    "star": 4,
+    "rateCount": 0,
+    "promo": {
+        "name": "giamgia",
+        "value": "10.000"
+    },
+    "detail": {
+       
+    },
+    "masp": "nhabanu"
+}, {
+    "name": "HARRY POTTER VÀ HÒN ĐÁ PHÙ THỦY ",
+    "company": "CGV",
+    "img": "img/products/9.jpg",
+    "price": "200.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+       
+    },
+    "masp": "harrypotter"
+}, {
+    "name": "Bố Già",
+    "company": "GalaxyCinema",
+    "img": "img/products/10.jpg",
+    "price": "2.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+       
+    },
+    "masp": "bogia"
+}]
